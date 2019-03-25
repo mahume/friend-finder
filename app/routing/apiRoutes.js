35a34bc82a4d896ecd731535f3913b03c0b02561
mahume <michael.a.hume@gmail.com>
2019-03-24 19:25:12 -0600

    done

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,23 +7,23 @@ router.get('/api/friends', (req, res) => {
 })
 
 router.post('/api/friends', (req, res) => {
-    console.log(req.body)
-    console.log(res)
-    friendsData.push(req.body)
+    const userScores = req.body
+    let bestScore = 40
+    let newFriend
 
-
+    for (let i = 0; i < friendsData.length; i++) {
+        const currentScore = totalDifference(friendsData[i].scores, userScores)
+        if (currentScore <= bestScore) {
+            bestScore = currentScore
+        }
+    }
 
 })
-
-module.exports = router
-/*
-const testAnswers1 = [5, 5, 2, 4, 1, 2, 4, 1, 2, 4]
-const testAnswers2 = [1, 2, 3, 4, 2, 2, 5, 3, 1, 4]
 
 function totalDifference(arr1, arr2) {
     arr1Sum = arr1.reduce((acc, cur) => acc + cur)
     arr2Sum = arr2.reduce((acc, cur) => acc + cur)
     console.log(Math.abs(arr1Sum - arr2Sum))
 }
-totalDifference(testAnswers1, testAnswers2)
-*/
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
+const apiRoutes = require('./app/routing/apiRoutes')
+const htmlRoutes = require('./app/routing/htmlRoutes')
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(apiRoutes)
+app.use(htmlRoutes)
 
-app.use(require('./app/routing/apiRoutes'))
-app.use(require('./app/routing/htmlRoutes'))
-
-app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`))
