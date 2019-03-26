4deba188f962858715352ac7880454b0fafa933d
mahume <michael.a.hume@gmail.com>
2019-03-25 20:22:32 -0600

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,33 +1,33 @@
-let friends = [
+const friends = [
     {
-        name: "Jerry Seinfeld",
-        photo: "images/jerry-seinfeld.jpg",
-        desc: `Jerry is the voice of reason. Observational by nature and quite the optimist. Yet be careful because he's always bringing new women around. Also he's a total neat freak so stay organized around him.`,
-        scores: [5, 4, 5, 1, 2, 5, 3, 3, 2, 5]
+        name: `Jerry Seinfeld`,
+        photo: `assets/images/jerry-seinfeld.jpg`,
+        desc: `Jerry is the voice of reason. Observational by nature and quite the optimist. Yet be careful because he's always bringing new women around. Also, he's a total neat freak so stay organized around him.`,
+        scores: [4, 5, 1, 2, 1, 5, 3, 1, 3, 5]
     },
     {
-        name: 'George Costanza',
-        photo: 'images/george-costanza.jpg',
-        desc: `George is a neurotic, bitter, selfish individual. He possesses few redeeming qualities. But if you need someone to lie and get you out of a sticky situation, he'll be your best new bud.`,
-        scores: [2, 3, 1, 4, 5, 1, 4, 5, 4, 2]
+        name: `George Costanza`,
+        photo: `assets/images/george-costanza.jpg`,
+        desc: `George is a neurotic, bitter, selfish individual. He possesses few redeeming qualities. But if you need a professional liar to get you out of a sticky situation, he'll be the perfect sidekick.`,
+        scores: [2, 2, 4, 4, 5, 1, 5, 5, 5, 2]
     },
     {
-        name: 'Elaine Benes',
-        photo: 'images/elaine-benes.jpg',
-        desc: `Elaine is intelligent yet cynical and outspoken. She has trouble maintaining a relationship due to shallow reasons. Perhaps it has something to do with her terrible dance moves.`,
-        scores: [1, 5, 4, 2, 1, 4, 5, 2, 4, 4]
+        name: `Elaine Benes`,
+        photo: `assets/images/elaine-benes.jpg`,
+        desc: `Elaine is intelligent, cynical and outspoken. She has trouble maintaining a relationship due to shallow reasons. Perhaps it has something to do with her terrible dance moves. Although, if you're looking for an extrovert to hang around with, she's the life of the party.`,
+        scores: [1, 4, 1, 2, 1, 5, 5, 1, 5, 2]
     },
     {
-        name: 'Cosmo Kramer',
-        photo: 'images/cosmo-kramer.jpg',
-        desc: `Outgoing, eccentric and kooky, Cosmo is a friend that will tell you how it is. His personality might raise a few eyebrows but once you understand him, you'll grow to love him. Be careful though, hanging out with this character might land you in some situations you never expected to occur.`,
-        scores: [3, 1, 3, 5, 2, 3, 1, 1, 2, 3]
+        name: `Cosmo Kramer`,
+        photo: `assets/images/cosmo-kramer.jpg`,
+        desc: `Outgoing, eccentric and kooky, Kramer is a friend that will tell you how it is. His personality might raise a few eyebrows but once you understand him, you'll grow to love him. Be careful though, hanging out with this character might land you in some situations you would have never expected were possible.`,
+        scores: [3, 1, 2, 5, 2, 3, 1, 1, 1, 3]
     },
     {
-        name: 'Newman',
-        photo: 'images/newman.jpg',
-        desc: `If you're looking for someone to tag along, Newman is your guy. Watch out because he's always scheming. Some may call him cunning and weasely, but if you're a woman he will follow you like a loyal dog.`,
-        scores: [2, 2, 1, 1, 3, 1, 2, 3, 1, 1]
+        name: `Newman`,
+        photo: `assets/images/newman.jpg`,
+        desc: `If you're looking for someone to tag along, Newman is your guy. Watch out though, because he's always scheming. Some may think of him as cunning and weasel-like, but if you're a woman he will follow you like a loyal dog.`,
+        scores: [2, 1, 5, 1, 2, 1, 2, 3, 1, 1]
     }
 ]
 
